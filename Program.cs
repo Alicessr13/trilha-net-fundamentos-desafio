6d56cb3f190ac7f5367f985467d1b27397c5f60c
Alice <alicessr12@gmail.com>
2024-09-01 16:46:11 -0300

    Desafio sistema de estacionamento

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using trilha_net_fundamentos_desafio.models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+Console.WriteLine("Digite o preço inicial: ");
+string input = Console.ReadLine();
+
+decimal precoInicial;
+
+decimal.TryParse(input, out precoInicial);
+
+Console.WriteLine("Digite o preço por hora: ");
+string input2 = Console.ReadLine();
+
+decimal precoPorHora;
+
+decimal.TryParse(input2, out precoPorHora);
+
+Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+bool exibirMenu = true;
+string opcao;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            break;
+
+        case "2":
+            estacionamento.RemoverVeiculo();
+            break;
+
+        case "3":
+            estacionamento.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou!");
